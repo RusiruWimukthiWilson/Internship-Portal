bdab5311a950c483aaa46330a5fe7480a5b58512
Ruzi <rspr79760@gmail.com>
2026-06-22 20:13:27 +0530

    Create Role.java

---
+++ src/main/java/com/portal/internship/model/Role.java
@@ -0,0 +1,20 @@
+package com.portal.internship.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name; // e.g., "ROLE_STUDENT", "ROLE_COMPANY", "ROLE_ADMIN"
+}
